a34813da14b6fc5381b383bf6e4554e6918381d5
藕霸 <zhangtao@rightknights.com>
2024-01-08 15:37:56 +0800

    更新测试版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.2.3")
+    version.set("2023.3.2")
     type.set("IU") // Target IDE Platform
 
     plugins.set(listOf(
