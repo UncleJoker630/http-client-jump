39b7fd7ac4f3bee45467489b42abd75f43e54949
藕霸 <zhangtao@rightknights.com>
2024-01-15 10:32:36 +0800

    un

--- src/main/java/com/joker/httpclientjump/constant/SpringClassNames.java
+++ src/main/java/com/joker/httpclientjump/constant/SpringClassNames.java
@@ -13,17 +13,15 @@ import java.util.Set;
  */
 public interface SpringClassNames {
 
-  String REQUEST = "org.springframework.web.bind.annotation.RequestMapping";
-  String REQUEST_GET = "org.springframework.web.bind.annotation.GetMapping";
-  String REQUEST_PUT = "org.springframework.web.bind.annotation.PutMapping";
-  String REQUEST_POST = "org.springframework.web.bind.annotation.PostMapping";
-  String REQUEST_PATCH = "org.springframework.web.bind.annotation.PatchMapping";
-  String REQUEST_DELETE = "org.springframework.web.bind.annotation.DeleteMapping";
-  Set<String> REQUEST_COLLECTION = Sets
-      .newHashSet(REQUEST, REQUEST_GET, REQUEST_PUT, REQUEST_POST, REQUEST_PATCH, REQUEST_DELETE);
-
-  String CONTROLLER = "org.springframework.stereotype.Controller";
-  String REST_CONTROLLER = "org.springframework.web.bind.annotation.RestController";
-  Set<String> CONTROLLER_COLLECTION = Sets.newHashSet(CONTROLLER, REST_CONTROLLER);
+    String REQUEST = "org.springframework.web.bind.annotation.RequestMapping";
+    String REQUEST_GET = "org.springframework.web.bind.annotation.GetMapping";
+    String REQUEST_PUT = "org.springframework.web.bind.annotation.PutMapping";
+    String REQUEST_POST = "org.springframework.web.bind.annotation.PostMapping";
+    String REQUEST_PATCH = "org.springframework.web.bind.annotation.PatchMapping";
+    String REQUEST_DELETE = "org.springframework.web.bind.annotation.DeleteMapping";
+    Set<String> REQUEST_COLLECTION = Sets.newHashSet(REQUEST, REQUEST_GET, REQUEST_PUT, REQUEST_POST, REQUEST_PATCH, REQUEST_DELETE);
+    String CONTROLLER = "org.springframework.stereotype.Controller";
+    String REST_CONTROLLER = "org.springframework.web.bind.annotation.RestController";
+    Set<String> CONTROLLER_COLLECTION = Sets.newHashSet(CONTROLLER, REST_CONTROLLER);
 
 }
